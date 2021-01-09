4cfa363ec99dbcce71af5c5c2c63289710714857
mahonya333 <mahonya333@gmail.com>
2021-01-09 17:38:20 +0200

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -2,15 +2,81 @@ $(function () {
 
 });
 
-const slider = document.querySelector('.swiper-container');
-/* Старт swiper-slider на странице ГЛАВНАЯ */
+const sliderNav = document.querySelector('.header-swiper-nav .swiper-container');
+const slider = document.querySelector('.header-swiper .swiper-container');
 let mySwiper = new Swiper(slider, {
-    slidesPerView: 3,
-    spaceBetween: 55,
+    slidesPerView: 1,/* Количество слайдов для одновременного показа*/
+    /* loop: true, *//* Бессконечная прокрутка */
+    loopedSlides: 12,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',/* Кнопкиа навигации */
+        prevEl: '.swiper-button-prev',/* Кнопка навигации */
     },
+    observer: true,
+    observeParents: true,
+    observeSlideChildren: true,
 
 })
+/* Старт swiper-slider на странице ГЛАВНАЯ */
+let mySwiperNav = new Swiper(sliderNav, {
+    slidesPerView: 3,/* Количество слайдов для одновременного показа*/
+    /* loop: true, *//* Бессконечная прокрутка */
+    loopedSlides: 12,
+    spaceBetween: 55,/* Отступы между слайдами */
+    navigation: {
+        nextEl: '.swiper-button-next',/* Кнопкиа навигации */
+        prevEl: '.swiper-button-prev',/* Кнопка навигации */
+    },
+
+    observer: true,
+    observeParents: true,
+    observeSlideChildren: true,
+
+    slideToClickedSlide: true,/* Переходим к кдикнутому слайду */
+
+    thumbs: {
+        swiper: mySwiper
+    }
+
+})
+
+function filterGames() {
+    const buttons = document.querySelectorAll('.header-content-tab-btn');/* Заносм в массив кнопки переключатели */
+    const cardsGame = document.querySelectorAll('.header .swiper-slide'); /* Заносм в массив карточки игр */
+
+    function filter(category, items) {
+        items.forEach((item) => {/* Проходим в цыкле по всем карточкам */
+            const isItemFiltered = !item.classList.contains(category);/* Ищем в класс-листе карточки текущую категорию*/
+            const isShowAll = category.toLowerCase() === 'all';
+            if (isItemFiltered && !isShowAll) {/* Если нашли, то*/
+                item.classList.add('hide');
+                mySwiper. update();
+                mySwiperNav. update();
+
+                mySwiper.init();
+                mySwiperNav.init();
+            }
+            else {    
+                item.classList.remove('hide');
+                mySwiper. update();
+                mySwiperNav. update();
+
+                mySwiper.init();
+                mySwiperNav.init();
+/*                 mySwiper.destroy();
+                mySwiperNav.destroy(); */
+            }
+        })
+    }
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const currentCategory = button.dataset.filter;/* Берем значение атрибута data-filter у нажатой кнопки и заносим в константу */
+            filter(currentCategory, cardsGame); /* Вызываем функцию фильтрации карточек*/
+        })
+    })
+}
+filterGames()
+
+
 /* Конец swiper-slider на странице ГЛАВНАЯ */
